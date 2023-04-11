06124e7bab4bcb85d9c06446395572c334859373
Emma Gerigscott <elgerig@gmail.com>
2023-04-11 11:02:57 -0700

    test 5 passes makes an array length of 9

--- __tests__/check.test.js
+++ __tests__/check.test.js
@@ -25,4 +25,44 @@ describe('Sudoku', () => {
     const sudoku = new Sudoku(rows);
     expect(sudoku.rows.length).toEqual(9);
   });
+
+  test('should return an array when columns are called', () => {
+    const rows = [];
+    const sudoku = new Sudoku(rows);
+    expect(Array.isArray(sudoku.columns)).toEqual(true);
+  });
+
+  test('should create columns array when method .makeColumns() is called on instance of sudoku object', () => {
+    const rows = [];
+    const row1 = [1,2,3,4,5,6,7,8,9];
+    const row2 = [1,2,3,4,5,6,7,8,9];
+    const row3 = [1,2,3,4,5,6,7,8,9];
+    const row4 = [1,2,3,4,5,6,7,8,9];
+    const row5 = [1,2,3,4,5,6,7,8,9];
+    const row6 = [1,2,3,4,5,6,7,8,9];
+    const row7 = [1,2,3,4,5,6,7,8,9];
+    const row8 = [1,2,3,4,5,6,7,8,9];
+    const row9 = [1,2,3,4,5,6,7,8,9];
+    rows.push(row1, row2, row3, row4, row5, row6, row7, row8, row9);
+    const sudoku = new Sudoku(rows)
+    sudoku.makeColumns();
+    expect(Array.isArray(sudoku.columns)).toEqual(true);
+  });
+
+    test('should create 9 arrays inside column array when .makeColumns() is called on instance of sudoku object', () => {
+      const rows = [];
+      const row1 = [1,2,3,4,5,6,7,8,9];
+      const row2 = [1,2,3,4,5,6,7,8,9];
+      const row3 = [1,2,3,4,5,6,7,8,9];
+      const row4 = [1,2,3,4,5,6,7,8,9];
+      const row5 = [1,2,3,4,5,6,7,8,9];
+      const row6 = [1,2,3,4,5,6,7,8,9];
+      const row7 = [1,2,3,4,5,6,7,8,9];
+      const row8 = [1,2,3,4,5,6,7,8,9];
+      const row9 = [1,2,3,4,5,6,7,8,9];
+      rows.push(row1, row2, row3, row4, row5, row6, row7, row8, row9);
+      const sudoku = new Sudoku(rows)
+      sudoku.makeColumns();
+      expect(sudoku.columns.length).toEqual(9)
+    });
 });
--- src/check.js
+++ src/check.js
@@ -1,3 +1,17 @@
 export default function Sudoku(rows) {
   this.rows = rows
+  this.columns = [];
+}
+
+Sudoku.prototype.makeColumns = function() {
+  const column1 = this.rows.map((row) => row[0]);
+  const column2 = this.rows.map((row) => row[1]);
+  const column3 = this.rows.map((row) => row[2]);
+  const column4 = this.rows.map((row) => row[3]);
+  const column5 = this.rows.map((row) => row[4]);
+  const column6 = this.rows.map((row) => row[5]);
+  const column7 = this.rows.map((row) => row[6]);
+  const column8 = this.rows.map((row) => row[7]);
+  const column9 = this.rows.map((row) => row[8]);
+  this.columns.push(column1, column2, column3, column4, column5, column6, column7, column8, column9);
 }
