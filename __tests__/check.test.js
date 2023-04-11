fddd159a56ab1edc57f44a1cf3c86beac774146b
Emma Gerigscott <elgerig@gmail.com>
2023-04-11 10:36:48 -0700

    update test so that rows key is an array of arrays

--- __tests__/check.test.js
+++ __tests__/check.test.js
@@ -4,8 +4,10 @@ describe('Sudoku', () => {
 
   test('should create a row with a length of 9', () => {
     const row1 = [1,2,3,4,5,6,7,8,9];
-    const sudoku = new Sudoku(row1);
-    expect(sudoku.row1.length).toEqual(9);
+    const rows = [];
+    rows.push(row1);
+    const sudoku = new Sudoku(rows);
+    expect(sudoku.rows[0].length).toEqual(9);
   });
 
   test('should create nine rows', () => {
@@ -18,7 +20,9 @@ describe('Sudoku', () => {
     const row7 = [1,2,3,4,5,6,7,8,9];
     const row8 = [1,2,3,4,5,6,7,8,9];
     const row9 = [1,2,3,4,5,6,7,8,9];
-    const sudoku = new Sudoku(row1, row2, row3, row4, row5, row6, row7, row8, row9);
-    expect(Object.keys(sudoku).length).toEqual(9);
+    const rows = [];
+    rows.push(row1, row2, row3, row4, row5, row6, row7, row8, row9)
+    const sudoku = new Sudoku(rows);
+    expect(sudoku.rows.length).toEqual(9);
   });
 });
