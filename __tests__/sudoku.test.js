c6e18f008fdd5c70d82176fdf61712ee2de492d4
Emma Gerigscott <elgerig@gmail.com>
2023-04-12 11:06:44 -0700

    pas test 2 for sudoku, id property added to each instance of row

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -10,16 +10,16 @@ describe('Sudoku', () => {
     row = [1,2,3,4,5,6,7,8,9];
     newRow = new Row(row);
     sudoku = new Sudoku();
+    sudoku.addRow(newRow);
   })
 
   test('should add row to instance of sudoku that is an object that includes an array of length 9', () => {
-    sudoku.addRow(newRow);
     expect(sudoku.rows).toEqual({1: {row: [1,2,3,4,5,6,7,8,9]}});
   });
 
-  // test('should create nine rows', () => {
-  //   expect(sudoku.rows.length).toEqual(9);
-  // });
+  test('should have property of id', () => {
+    expect(sudoku.id).toEqual(1);
+  });
 
   // test('should return an array when columns are called', () => {
   //   expect(Array.isArray(sudoku.columns)).toEqual(true);
--- src/js/sudoku.js
+++ src/js/sudoku.js
@@ -2,11 +2,18 @@ export default class Sudoku {
   constructor() {
     this.rows = {};
     this.columns = {};
+    this.id = 0
   }
 
   addRow(row) {
-    let id = 1;
-    this.rows[id] = row;
+    this.rows[this.assignId()] = row;
+  }
+
+  assignId() {
+    if (this.id <= 9) {
+      this.id++;
+    } else { this.id = 0 }
+    return this.id
   }
   
   makeColumns() {
